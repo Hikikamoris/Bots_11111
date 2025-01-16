e1f1383de8cbe6a15db0608f3963c87f9597d5e9
Hikikamoris <99737130+Hikikamoris@users.noreply.github.com>
2025-01-17 02:23:54 +0300

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,50 +1,41 @@
-import time
-import feedparser
 import asyncio
-from telegram import Bot
+import feedparser
 import re
-from datetime import datetime, timedelta
+from aiogram import Bot, Dispatcher
+from aiogram.types import ParseMode
+from aiogram.utils import executor
+import os
+import time
 
 TOKEN = "8099129290:AAGptZmiE0jSbqami7mcO58AZHxoTUX-UZU"
 CHANNEL_ID = "@vzglad_today"
 
 RSS_FEED_URLS = [
-    "https://russian.rt.com/rss", 
+    "https://russian.rt.com/rss",
     "https://www.rbc.ru/rbcfreenews/index.rss",
 ]
 
 bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
-# Словарь для хранения ссылок с временными метками
-published_links = {}
-# Время, после которого посты считаются старыми
-TIME_LIMIT = timedelta(hours=12)
-
-def clean_html(text):
-    text = re.sub(r'<br>', '\n', text)
-    text = re.sub(r'<[^>]+>', '', text)
-    return text
-
-def remove_read_more(text):
-    text = re.sub(r'Читать далее.*', '', text)
-    return text
-
+# Проверим, есть ли сообщения, которые были опубликованы в последние 12 часов
 async def fetch_past_posts():
-    """Получаем ссылки из последних постов канала для фильтрации уже опубликованных ссылок."""
-    global published_links
-    # Получаем последние 100 сообщений из канала
+    current_time = time.time()
+    cutoff_time = current_time - 12 * 3600  # 12 часов назад
     messages = await bot.get_chat_history(CHANNEL_ID, limit=100)
-    
-    for message in messages:
-        # Если в сообщении есть ссылка
-        if message.text:
-            match = re.search(r'https?://[^\s]+', message.text)
-            if match:
-                link = match.group(0)
-                published_links[link] = datetime.now()  # Добавляем в published_links
 
+    past_links = set()
+    for message in messages:
+        if message.date.timestamp() > cutoff_time:  # Проверка по времени
+            if message.entities and message.entities[0].type == 'url':
+                link = message.text[message.entities[0].offset:message.entities[0].offset + message.entities[0].length]
+                past_links.add(link)
+    return past_links
+
+# Получение и отправка новых постов
 async def fetch_and_post():
-    global published_links
+    past_links = await fetch_past_posts()
+    
     for rss_feed_url in RSS_FEED_URLS:
         feed = feedparser.parse(rss_feed_url)
         if feed.bozo:
@@ -52,45 +43,45 @@ async def fetch_and_post():
             continue
 
         for entry in feed.entries:
-            link = entry.link
-            # Проверяем, была ли уже опубликована ссылка в последние 12 часов
-            if link in published_links:
-                last_published_time = published_links[link]
-                if datetime.now() - last_published_time < TIME_LIMIT:
-                    continue  # Пропускаем, если ссылка была опубликована менее 12 часов назад
+            if entry.link not in past_links:
+                clean_title = clean_html(entry.title)
+                clean_summary = clean_html(entry.summary)
+                clean_summary = remove_read_more(clean_summary)
 
-            clean_title = clean_html(entry.title)
-            clean_summary = clean_html(entry.summary)
-            clean_summary = remove_read_more(clean_summary)
+                message = f"<b>{clean_title}</b>\n\n{clean_summary}\n\n<a href='{entry.link}'>Читать далее</a>"
 
-            message = f"<b>{clean_title}</b>\n\n{clean_summary}\n\n<a href='{link}'>Читать далее</a>"
+                if 'media_content' in entry:
+                    for media in entry['media_content']:
+                        if 'url' in media:
+                            image_url = media['url']
+                            await bot.send_photo(chat_id=CHANNEL_ID, photo=image_url, caption=message, parse_mode=ParseMode.HTML)
+                            break
+                else:
+                    await bot.send_message(chat_id=CHANNEL_ID, text=message, parse_mode=ParseMode.HTML)
 
-            if 'media_content' in entry:
-                for media in entry['media_content']:
-                    if 'url' in media:
-                        image_url = media['url']
-                        await bot.send_photo(chat_id=CHANNEL_ID, photo=image_url, caption=message, parse_mode="HTML")
-                        break
-            else:
-                await bot.send_message(chat_id=CHANNEL_ID, text=message, parse_mode="HTML")
+                await asyncio.sleep(30)  # Задержка между отправками
 
-            # Обновляем временную метку публикации
-            published_links[link] = datetime.now()
+# Очищаем HTML-теги
+def clean_html(text):
+    text = re.sub(r'<br>', '\n', text)
+    text = re.sub(r'<[^>]+>', '', text)
+    return text
 
-            await asyncio.sleep(30)
+# Убираем текст "Читать далее"
+def remove_read_more(text):
+    text = re.sub(r'Читать далее.*', '', text)
+    return text
 
+# Главная функция
 async def main():
     print("Бот запущен и работает!")
-    
-    # Загружаем уже опубликованные посты
-    await fetch_past_posts()
-    
     while True:
         await fetch_and_post()
-        await asyncio.sleep(600)
+        await asyncio.sleep(600)  # Пауза в 10 минут
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    executor.start_polling(dp, skip_updates=True)
+
 
 
 
