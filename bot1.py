4f0921143839c44e885e60d45a93b4e43431b4ed
Hikikamoris <99737130+Hikikamoris@users.noreply.github.com>
2025-01-17 01:36:28 +0300

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -13,7 +13,6 @@ CHANNEL_ID = "@vzglad_today"
 # Список URL RSS-источников
 RSS_FEED_URLS = [
     "https://russian.rt.com/rss",  # Пример 1
-    "https://lenta.ru/rss/google-newsstand/main/",  # Пример 2
     "https://www.rbc.ru/rbcfreenews/index.rss",  # Пример 3
     # Добавьте сюда другие источники
 ]
