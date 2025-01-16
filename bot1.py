859300a2f02f90510c47c9d802292ab4d5aefba9
Hikikamoris <99737130+Hikikamoris@users.noreply.github.com>
2025-01-17 02:01:18 +0300

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,10 +1,9 @@
 import time
 import feedparser
 import asyncio
-import json
+import redis
 from telegram import Bot
 import re
-import os
 
 TOKEN = "8099129290:AAGptZmiE0jSbqami7mcO58AZHxoTUX-UZU"
 CHANNEL_ID = "@vzglad_today"
@@ -14,24 +13,22 @@ RSS_FEED_URLS = [
     "https://www.rbc.ru/rbcfreenews/index.rss",
 ]
 
-PUBLISHED_LINKS_FILE = "published_links.json"
+REDIS_HOST = "localhost"  # Или используйте адрес хоста в облаке
+REDIS_PORT = 6379
+REDIS_DB = 0
 
 bot = Bot(token=TOKEN)
 
+# Подключение к Redis
+redis_client = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True)
+
 def load_published_links():
-    if os.path.exists(PUBLISHED_LINKS_FILE):
-        try:
-            with open(PUBLISHED_LINKS_FILE, "r") as file:
-                return set(json.load(file))
-        except json.JSONDecodeError:
-            return set()
-    return set()
+    # Получаем список ссылок из Redis (если они есть)
+    return set(redis_client.smembers("published_links"))
 
 def save_published_links(links):
-    with open(PUBLISHED_LINKS_FILE, "w") as file:
-        json.dump(list(links), file)
-
-published_links = load_published_links()
+    # Сохраняем ссылки в Redis
+    redis_client.sadd("published_links", *links)
 
 def clean_html(text):
     text = re.sub(r'<br>', '\n', text)
@@ -43,7 +40,7 @@ def remove_read_more(text):
     return text
 
 async def fetch_and_post():
-    global published_links
+    published_links = load_published_links()  # Получаем текущие опубликованные ссылки
     for rss_feed_url in RSS_FEED_URLS:
         feed = feedparser.parse(rss_feed_url)
         if feed.bozo:
@@ -67,9 +64,7 @@ async def fetch_and_post():
                 else:
                     await bot.send_message(chat_id=CHANNEL_ID, text=message, parse_mode="HTML")
 
-                published_links.add(entry.link)
-                save_published_links(published_links)
-
+                save_published_links([entry.link])  # Сохраняем новую ссылку в Redis
                 await asyncio.sleep(30)
 
 async def main():
@@ -80,6 +75,7 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 
 
 
